7cf954154609d9fca4e0769d97789194e3da934a
Arturo Bayangos <ambibayangos@gmail.com>
2023-04-18 00:46:21 +1200

    Make looping game and track score then declare winner

--- src.js
+++ src.js
@@ -5,6 +5,10 @@ const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
 
+const player_wins = 2;
+const bot_wins = 3;
+const draw = 0;
+
 
 function getComputerChoice(){
     let randomFloat = Math.random(); // create a float between 0-1 not including.
@@ -24,32 +28,41 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection) {
     let lowerCasePlayerSelection = playerSelection.toLowerCase();
     if(lowerCasePlayerSelection == computerSelection){
-        return "Draw!"
+        return draw;
     }
 
     if(lowerCasePlayerSelection == rock && computerSelection == paper){
-        return "Player wins - paper beats rock :)"
+        return player_wins;
     }else if(computerSelection = rock && lowerCasePlayerSelection==paper){
-        return "Bot wins - paper beats rock :("
+        return bot_wins;
     }
 
     if(lowerCasePlayerSelection == scissors && computerSelection == paper){
-        return "Player wins - scissor beats paper :)"
+        return player_wins;
     }else if(computerSelection = scissors && lowerCasePlayerSelection==paper){
-        return "Bot wins - scissor beats paper :("
+        return bot_wins;
     }
 
     if(lowerCasePlayerSelection == paper && computerSelection == rock){
-        return "Player wins - paper beats rock :)"
+        return player_wins;
     }else if(computerSelection = paper && lowerCasePlayerSelection==rock){
-        return "Bot wins - paper beats rock :("
+        return bot_wins;
     }
+  }
+
+  function Game(){
+    // call playGame() multiple time
+    let playerScore = 0;
+    let botScore = 0;
+    for(let i = 0; i < 10; i++){
+      const playerSelection = "rock";
+      const computerSelection = getComputerChoice();
+      let tempResult = playRound(playerSelection, computerSelection);
+      (tempResult == player_wins) && tempResult != draw ? playerScore++ : botScore++;
+    }
+    playerScore > botScore ? console.log("Player Wins :)") : console.log("Bot Wins :(");
   }
    
 
-  for(let i = 0; i < 10; i++){
-    const playerSelection = "rock";
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
- }
- 
+Game();
+
